64bb342607c64f13bcd1d1629780609a8168911b
Beth <bethhnielsen56@gmail.com>
2021-09-14 14:58:31 -0500

    added problem to 8kyu

--- 7kyuProblems.js
+++ 7kyuProblems.js
@@ -1,5 +1,5 @@
 // PROBLEM- codewars Last Survivor
-//
+
 // You are given a string of letters and an array of numbers.
 // The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
 // After each removal the size of the string decreases (there is no empty space).
@@ -14,10 +14,8 @@
 // SOLUTION
 const lastSurvivor = (letters, coords) => {
   // converts string to array of single letters
-  // ['a', 'b', 'c']
   const lettersArr = letters.split('');
-  // loops through numbers in coords array
-  // each number removes 1 string at position x
+  // loops through numbers in coords array each number removes 1 string at position x
   coords.forEach((x) => lettersArr.splice([x], 1));
   // returns the remaining letter as a string
   return lettersArr.toString();
---
+++ 8kyuProblems.js
@@ -0,0 +1,7 @@
+// PROBLEM codewars Beginner Series #4 Cockroach
+
+//The cockroach is one of the fastest insects. 
+// Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer.
+
+// Solution
+const cockroachSpeed = (s) => Math.floor(s*27.7778) 
