a6691f10ba88c21f47fbb2bf773ce7c7af7c30ae
Beth <bethhnielsen56@gmail.com>
2021-09-28 20:37:30 -0500

    added new problem

---
+++ 8kyu/ReversedString.js
@@ -0,0 +1,13 @@
+// PROBLEM Reversed String 
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+
+// SOLUTION
+function solution(str){
+  const newArray = [];
+	const stringToArray = str.split('');
+ 	for(let i = stringToArray.length - 1; i >= 0; i--) {
+    newArray.push(stringToArray[i]);
+  }
+  return newArray.join('');
+}
